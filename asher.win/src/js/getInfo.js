b627ec8f6b3dc69053bf04be0549bb73dabbdd41
Mike <pacstrap@mx.idalon.org>
2024-08-24 17:21:46 +1000

    add json interprator.

---
+++ asher.win/src/js/getInfo.js
@@ -0,0 +1,17 @@
+async function fetchDataAndUpdateContent() {
+    try {
+      const response = await fetch(`http://${window.location.href.split("/")[2]}:9038/device-info`);
+      if (!response.ok) throw new Error('Network response was not ok');
+      const data = await response.json();
+
+      document.getElementById('siteName').textContent = "Home - " + data.displayName;
+      document.getElementById('welcomeToServerName').textContent = data.hostName;
+      document.getElementById('deviceModalIP').textContent = data.networking.ip;
+      document.getElementById('deviceModalSearchDomain').textContent = data.networking.searchDomain;
+  
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+fetchDataAndUpdateContent();
